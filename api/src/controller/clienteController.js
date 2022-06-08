e0bf72883dc7cae2ebc85d1f1a2848115e808cf1
Thawan Nascimento Silva <thawann.silva34@gmail.com>
2022-06-08 18:12:15 -0300

    Subindo com uma nova estrutura

--- api/src/controller/clienteController.js
+++ api/src/controller/clienteController.js
@@ -10,20 +10,44 @@ server.post('/cliente' , async (req, resp) => {
     try {
         const cliente = req.body;
 
-        if(!cliente.personal) throw new Error ('Id do personal obrigatório');
-        if(!cliente.nome) throw new Error ('Nome do cliente é obrigatório');
-        if(!cliente.plano) throw new Error ('Nome do plano é obrigatório');
-        if(!cliente.cpf) throw new Error ('CPF é obrigatório');
-        if(!cliente.genero) throw new Error ('Gênero é obrigatório');
-        if(cliente.nascimento == undefined) throw new Error ('Data de nascimento é obrigatória');
-        if(cliente.altura == undefined || cliente.altura < 0) throw new Error ('Altura obrigatória');
-        if(cliente.peso == undefined || cliente.peso < 0) throw new Error ('Peso obrigatório');
-        if(!cliente.telefone) throw new Error ('Telefone obrigatório');
-        if(!cliente.objetivo) throw new Error ('Objetivo obrigatório');
-        if(!cliente.observacao) throw new Error ('Observação obrigatória');
-        if(!cliente.treino) throw new Error ('Informações  sobre treino obrigatórias');
-        if(!cliente.dia) throw new Error ('Dia da semana obrigatório');
-        if(!cliente.horario) throw new Error ('Horário obrigatório');
+        if(!cliente.personal) 
+            throw new Error ('Id do personal obrigatório');
+
+        if(!cliente.nome) 
+            throw new Error ('Nome do cliente é obrigatório');
+
+        if(!cliente.plano) 
+            throw new Error ('Nome do plano é obrigatório');
+
+        if(!cliente.cpf) 
+            throw new Error ('CPF é obrigatório');
+
+        if(!cliente.genero) 
+            throw new Error ('Gênero é obrigatório');
+
+        if(cliente.nascimento == undefined) 
+            throw new Error ('Data de nascimento é obrigatória');
+
+        if(cliente.altura == undefined || cliente.altura < 0) 
+            throw new Error ('Altura obrigatória');
+
+        if(cliente.peso == undefined || cliente.peso < 0) 
+            throw new Error ('Peso obrigatório');
+
+        if(!cliente.telefone) 
+            throw new Error ('Telefone obrigatório');
+
+        if(!cliente.objetivo) 
+            throw new Error ('Objetivo obrigatório');
+
+        if(!cliente.treino) 
+            throw new Error ('Informações sobre treino obrigatórias');
+
+        if(!cliente.dia) 
+            throw new Error ('Dia da semana obrigatório');
+
+        if(!cliente.horario) 
+            throw new Error ('Horário obrigatório');
 
         const clienteAdcionado = await inserirCliente(cliente);
         resp.send(clienteAdcionado);
@@ -79,20 +103,44 @@ server.put('/cliente/:id' , async (req, resp) => {
         const { id } = req.params;
         const cliente = req.body;
 
-        if(!cliente.personal) throw new Error ('Id do personal obrigatório');
-        if(!cliente.nome) throw new Error ('Nome do cliente é obrigatório');
-        if(!cliente.plano) throw new Error ('Nome do plano é obrigatório');
-        if(!cliente.cpf) throw new Error ('CPF é obrigatório');
-        if(!cliente.genero) throw new Error ('Gênero é obrigatório');
-        if(cliente.nascimento == undefined) throw new Error ('Data de nascimento é obrigatória');
-        if(cliente.altura == undefined || cliente.altura < 0) throw new Error ('Altura obrigatória');
-        if(cliente.peso == undefined || cliente.peso < 0) throw new Error ('Peso obrigatório');
-        if(!cliente.telefone) throw new Error ('Telefone obrigatório');
-        if(!cliente.objetivo) throw new Error ('Objetivo obrigatório');
-        if(!cliente.observacao) throw new Error ('Observação obrigatória');
-        if(!cliente.treino) throw new Error ('Informações  sobre treino obrigatórias');
-        if(!cliente.dia) throw new Error ('Dia da semana obrigatório');
-        if(!cliente.horario) throw new Error ('Horário obrigatório');
+        if(!cliente.personal) 
+            throw new Error ('Id do personal obrigatório');
+
+        if(!cliente.nome) 
+            throw new Error ('Nome do cliente é obrigatório');
+
+        if(!cliente.plano) 
+            throw new Error ('Nome do plano é obrigatório');
+
+        if(!cliente.cpf) 
+            throw new Error ('CPF é obrigatório');
+
+        if(!cliente.genero) 
+            throw new Error ('Gênero é obrigatório');
+
+        if(cliente.nascimento == undefined) 
+            throw new Error ('Data de nascimento é obrigatória');
+
+        if(cliente.altura == undefined || cliente.altura < 0) 
+            throw new Error ('Altura obrigatória');
+
+        if(cliente.peso == undefined || cliente.peso < 0) 
+            throw new Error ('Peso obrigatório');
+
+        if(!cliente.telefone) 
+            throw new Error ('Telefone obrigatório');
+
+        if(!cliente.objetivo) 
+            throw new Error ('Objetivo obrigatório');
+
+        if(!cliente.treino) 
+            throw new Error ('Informações  sobre treino obrigatórias');
+
+        if(!cliente.dia) 
+            throw new Error ('Dia da semana obrigatório');
+
+        if(!cliente.horario) 
+            throw new Error ('Horário obrigatório');
 
         const resposta = await alterarCliente(id, cliente);
         if (resposta != 1)
